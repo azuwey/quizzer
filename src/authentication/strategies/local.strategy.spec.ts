859f9b345ae54cb6a41471c9f2c404dbdf60dab2
Dávid János Zarándi <david.zarandi@pm.me>
2022-05-22 17:29:08 +0200

    Local strategy implementation and test

---
+++ src/authentication/strategies/local.strategy.spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { hashSync } from 'bcrypt';
+import { Types } from 'mongoose';
+import { LocalStrategy } from './local.strategy';
+import { Account } from '../../accounts/schemas/account.schema';
+import { BCRYPT } from '../../constants/constants';
+import { AuthenticationService } from '../authentication.service';
+import { UnauthorizedException } from '@nestjs/common';
+
+describe('LocalStrategy', () => {
+  let strategy: LocalStrategy;
+  let authenticationService: AuthenticationService;
+
+  const mockAccount: Account = {
+    _id: new Types.ObjectId('aaaaaaaaaaaaaaaaaaaaaaaa'),
+    emailAddress: 'test@test.com',
+    passwordHash: hashSync('test', BCRYPT.ROUNDS),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LocalStrategy,
+        {
+          provide: AuthenticationService,
+          useValue: {
+            validateUser: jest.fn().mockResolvedValue(mockAccount),
+          },
+        },
+      ],
+    }).compile();
+
+    strategy = module.get<LocalStrategy>(LocalStrategy);
+    authenticationService = module.get<AuthenticationService>(
+      AuthenticationService,
+    );
+  });
+
+  describe('validate', () => {
+    it('should return an account', async () => {
+      expect(await strategy.validate('test@test.com', 'test')).toEqual(
+        mockAccount,
+      );
+    });
+
+    it('should throw an error', async () => {
+      jest
+        .spyOn(authenticationService, 'validateUser')
+        .mockImplementationOnce(() => Promise.resolve(null));
+
+      await expect(
+        strategy.validate('test@test.com', 'test'),
+      ).rejects.toThrowError(UnauthorizedException);
+    });
+  });
+});
---
+++ src/authentication/strategies/local.strategy.ts
@@ -0,0 +1,24 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthenticationService } from '../authentication.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authenticationService: AuthenticationService) {
+    super();
+  }
+
+  async validate(emailAddress: string, password: string): Promise<any> {
+    const user = await this.authenticationService.validateUser(
+      emailAddress,
+      password,
+    );
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
